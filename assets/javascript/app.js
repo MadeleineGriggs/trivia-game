8088878693336feb75a97d22c4731a8c55ce1ff2
Madeleine Griggs <madeleinegriggs@gmail.com>
2019-06-17 17:51:56 -0400

    added shuffle function so possible answers appear in random order

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,11 +27,8 @@ var TriviaCategories = [
 $(document).ready(function() {
 
     var gameCategory = "";
-    var gameQuestion = "";
-    var answer1 = "";
-    var answer2 = "";
-    var answer3 = "";
-    var answer4 = "";
+    var wins = 0;
+    var losses = 0;
 
     gameStart();
 
@@ -81,13 +78,38 @@ $(document).ready(function() {
         }, 1000);
     }
 
+    function shuffle(a) {
+        for (let i = a.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [a[i], a[j]] = [a[j], a[i]];
+        }
+        return a;
+    }
+
     function triviaDisplay() {
         gameTimer();
-        var question = TriviaCategories[0]["questions"]["question1"];
-        var category = TriviaCategories.length;
-        var answer1 = TriviaCategories[0]["questions"]["answers"]["wrongAnswer1"];
+        var categoryIndex = 0;
+        if(gameCategory === "cats") {
+            categoryIndex = 0;
+        } else if (gameCategory === "dogs") {
+            categoryIndex = 1;
+        } else if (gameCategory === "other") {
+            categoryIndex = 2;
+        }
+        var question = TriviaCategories[categoryIndex]["questions"]["question1"];
+        var answerArray = [];
+        var answer1 = TriviaCategories[categoryIndex]["questions"]["answers"]["wrongAnswer1"];
+        var answer2 = TriviaCategories[categoryIndex]["questions"]["answers"]["wrongAnswer2"];
+        var answer3 = TriviaCategories[categoryIndex]["questions"]["answers"]["wrongAnswer3"];
+        var answer4 = TriviaCategories[categoryIndex]["questions"]["answers"]["correctAnswer"];
+        answerArray.push(answer1, answer2, answer3, answer4);
+        shuffle(answerArray);
+        console.log(answerArray);
         $(".question-container").text(question);
-        $("#ans-1-text").text(answer1);
+        $("#ans-1-text").text(answerArray[0]);
+        $("#ans-2-text").text(answerArray[1]);
+        $("#ans-3-text").text(answerArray[2]);
+        $("#ans-4-text").text(answerArray[3]);
     }
 
 
